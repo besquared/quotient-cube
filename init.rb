f8bcd898e3c15a830ba1b17868e822e9aa986ed3
Josh Ferguson <Josh@Besquared.local>
2009-11-03 03:30:36 -0800

    don't require vendor autobots

--- init.rb
+++ init.rb
@@ -3,7 +3,5 @@ require 'activesupport'
 require 'tokyocabinet'
 require 'yajl'
 
-require File.join(File.dirname(__FILE__), 'vendor', 'autobots-transform', 'init')
-
 require File.join(File.dirname(__FILE__), 'lib', 'fake_tokyo')
 require File.join(File.dirname(__FILE__), 'lib', 'quotient_cube')
